c15eaddb63d7c35bdc07e33debd737f3f8c5fa34
Marq Watkin <devhead@pmprog.co.uk>
2015-09-08 14:56:50 +0100

    Lobby

--- Game/gamelobbystage.cpp
+++ Game/gamelobbystage.cpp
@@ -9,16 +9,26 @@
 GameLobbyStage::GameLobbyStage()
 {
 	networkconnection = nullptr;
+	currentinfo = new GameInfo();
+	selection = 0;
 }
 
 GameLobbyStage::GameLobbyStage(Network* Connection)
 {
+	selection = 0;
 	networkconnection = Connection;
+	currentinfo = new GameInfo();
+
+	if( !Connection->IsServer() )
+	{
+		// TODO: Request gameinfo from server
+	}
 }
 
 void GameLobbyStage::Begin()
 {
 	optionfont = FontCache::LoadFont( "resources/armalite.ttf", 32 );
+	helpfont = FontCache::LoadFont( "resources/armalite.ttf", 16 );
 	// AUDIO->PlayMusic( "resources/Paul Hannay (Feekzoid) - Last_Ninja_4_loader [1].ogg", true );
 
 }
@@ -62,25 +72,104 @@ void GameLobbyStage::EventOccurred(Event *e)
 		inputevent = Input::GetItemFromEvent( e );
 	}
 
+	if( inputevent != InputItems::NONE )
+	{
+		InputEvent( inputevent );
+	}
+
+	NetworkEvent( e );
+
+}
+
+void GameLobbyStage::InputEvent(InputItems::ItemSet inputevent)
+{
 	switch( inputevent )
 	{
+		case InputItems::UP:
+			if( selection > 1 )
+			{
+				selection -= 2;
+			}
+			break;
+		case InputItems::DOWN:
+			if( selection < 4 )
+			{
+				selection += 2;
+			}
+			break;
+		case InputItems::LEFT:
+			if( (selection % 2) == 1 )
+			{
+				selection--;
+			}
+			break;
+		case InputItems::RIGHT:
+			if( (selection % 2) == 0 )
+			{
+				selection++;
+			}
+			break;
+
 		case InputItems::SELECT:
 			delete FRAMEWORK->ProgramStages->Pop();
 			return;
 			break;
 		case InputItems::X:
-			FRAMEWORK->ProgramStages->Push( new GameLobbyAddLocalStage( this ) );
+			if( networkconnection == nullptr )
+			{
+				FRAMEWORK->ProgramStages->Push( new GameLobbyAddLocalStage( this ) );
+			}
 			break;
 		case InputItems::B:
-			FRAMEWORK->ProgramStages->Push( new GameLobbyAddAIStage( this ) );
+			if( networkconnection == nullptr )
+			{
+				FRAMEWORK->ProgramStages->Push( new GameLobbyAddAIStage( this ) );
+			}
 			break;
 		case InputItems::Y:
-			FRAMEWORK->ProgramStages->Push( new GameLobbyKickStage( this ) );
+			if( networkconnection == nullptr || networkconnection->IsServer() )
+			{
+				FRAMEWORK->ProgramStages->Push( new GameLobbyKickStage( this ) );
+			}
 			break;
 		case InputItems::START:
-			//FRAMEWORK->ProgramStages->Push( new GameLobbyAddLocalStage( this ) );
+			
+			if( networkconnection == nullptr || networkconnection->IsServer() )
+			{
+				//FRAMEWORK->ProgramStages->Push( new GameStage( this ) );
+			}
 			break;
 
+	}
+}
+
+void GameLobbyStage::NetworkEvent(Event* e)
+{
+	if( e->Type == EVENT_NETWORK_CONNECTION_REQUEST )
+	{
+		bool freeslot = false;
+		freeslot = ( currentinfo->BlueA_Present ? true : freeslot );
+		freeslot = ( currentinfo->BlueB_Present ? true : freeslot );
+		freeslot = ( currentinfo->BlueC_Present ? true : freeslot );
+		freeslot = ( currentinfo->RedA_Present ? true : freeslot );
+		freeslot = ( currentinfo->RedB_Present ? true : freeslot );
+		freeslot = ( currentinfo->RedC_Present ? true : freeslot );
+		if( freeslot )
+		{
+			// TODO: Accept player and balance teams
+		}
+	}
+	if( e->Type == EVENT_NETWORK_CONNECTED )
+	{
+		// TODO: Send current game info
+	}
+	if( e->Type == EVENT_NETWORK_DISCONNECTED )
+	{
+		// TODO: Remove disconnected users
+	}
+	if( e->Type == EVENT_NETWORK_PACKET_RECEIVED )
+	{
+		// TODO: Process packet info
 	}
 }
 
@@ -98,44 +187,65 @@ void GameLobbyStage::Render()
 	GameResources::DrawPanel( GameResources::BluePanel, 5, 80, 13, 8, 8 );
 	optionfont->DrawString( 15, 85, "Blue Team", FontHAlign::LEFT, al_map_rgb( 255, 255, 255 ) );
 
-	GameResources::DrawButton( GameResources::WhiteButtonUp, 15, 140, 23, 3 );
-	GameResources::DrawButton( GameResources::WhiteButtonUp, 15, 192, 23, 3 );
-	GameResources::DrawButton( GameResources::WhiteButtonUp, 15, 244, 23, 3 );
+	RenderPlayerSlot( true, 0, currentinfo->BlueA_Present, currentinfo->BlueA_Name, currentinfo->BlueA_Local, currentinfo->BlueA_AI, 15, 140 );
+	RenderPlayerSlot( true, 2, currentinfo->BlueB_Present, currentinfo->BlueB_Name, currentinfo->BlueB_Local, currentinfo->BlueB_AI, 15, 192 );
+	RenderPlayerSlot( true, 4, currentinfo->BlueC_Present, currentinfo->BlueC_Name, currentinfo->BlueC_Local, currentinfo->BlueC_AI, 15, 244 );
 
 	GameResources::DrawPanel( GameResources::RedPanel, 405, 80, 13, 8, 8 );
 	optionfont->DrawString( 415, 85, "Red Team", FontHAlign::LEFT, al_map_rgb( 255, 255, 255 ) );
 
-	GameResources::DrawButton( GameResources::WhiteButtonUp, 415, 140, 23, 3 );
-	GameResources::DrawButton( GameResources::WhiteButtonUp, 415, 192, 23, 3 );
-	GameResources::DrawButton( GameResources::WhiteButtonUp, 415, 244, 23, 3 );
+	RenderPlayerSlot( false, 1, currentinfo->RedA_Present, currentinfo->RedA_Name, currentinfo->RedA_Local, currentinfo->RedA_AI, 415, 140 );
+	RenderPlayerSlot( false, 3, currentinfo->RedB_Present, currentinfo->RedB_Name, currentinfo->RedB_Local, currentinfo->RedB_AI, 415, 192 );
+	RenderPlayerSlot( false, 5, currentinfo->RedC_Present, currentinfo->RedC_Name, currentinfo->RedC_Local, currentinfo->RedC_AI, 415, 244 );
 
-	// Add Local Player
-	int textoff = (50 - optionfont->GetFontHeight()) / 2;
-	al_draw_bitmap( Input::GetIcon( InputItems::X ), 10, 320, 0 );
-	optionfont->DrawString( 64, 320 + textoff, "Add Local Player", FontHAlign::LEFT, al_map_rgb( 0, 0, 0 ) );
+	int textoff = (50 - helpfont->GetFontHeight()) / 2;
+	if( networkconnection == nullptr )
+	{
+		// Add Local Player
+		al_draw_bitmap( Input::GetIcon( InputItems::X ), 10, 320, 0 );
+		helpfont->DrawString( 64, 320 + textoff, "Add Local Player", FontHAlign::LEFT, al_map_rgb( 0, 0, 0 ) );
+
+		// Add AI
+		al_draw_bitmap( Input::GetIcon( InputItems::B ), 10, 370, 0 );
+		helpfont->DrawString( 64, 370 + textoff, "Add CPU Player", FontHAlign::LEFT, al_map_rgb( 0, 0, 0 ) );
+	}
 
 	if( networkconnection == nullptr || networkconnection->IsServer() )
 	{
-		// Add AI
-		al_draw_bitmap( Input::GetIcon( InputItems::B ), 10, 370, 0 );
-		optionfont->DrawString( 64, 370 + textoff, "Add CPU Player", FontHAlign::LEFT, al_map_rgb( 0, 0, 0 ) );
-
 		// Kick Player/AI
 		al_draw_bitmap( Input::GetIcon( InputItems::Y ), 10, 420, 0 );
-		optionfont->DrawString( 64, 420 + textoff, "Kick Player", FontHAlign::LEFT, al_map_rgb( 0, 0, 0 ) );
+		helpfont->DrawString( 64, 420 + textoff, "Kick Player", FontHAlign::LEFT, al_map_rgb( 0, 0, 0 ) );
 
 		// Start game
 		al_draw_bitmap( Input::GetIcon( InputItems::START ), 500, 370, 0 );
-		optionfont->DrawString( 554, 370 + textoff, "Start", FontHAlign::LEFT, al_map_rgb( 0, 0, 0 ) );
+		helpfont->DrawString( 554, 370 + textoff, "Start", FontHAlign::LEFT, al_map_rgb( 0, 0, 0 ) );
 	}
 
 	// Abort/Disconnect
 	al_draw_bitmap( Input::GetIcon( InputItems::SELECT ), 500, 420, 0 );
-	optionfont->DrawString( 554, 420 + textoff, "Quit", FontHAlign::LEFT, al_map_rgb( 0, 0, 0 ) );
+	helpfont->DrawString( 554, 420 + textoff, "Quit", FontHAlign::LEFT, al_map_rgb( 0, 0, 0 ) );
 
 }
 
 bool GameLobbyStage::IsTransition()
 {
 	return false;
+}
+
+void GameLobbyStage::RenderPlayerSlot(bool BlueTeam, int Index, bool Present, std::string Name, bool Local, bool AI, int X, int Y)
+{
+	SpriteSheet* btnimg = GameResources::WhiteButtonDown;
+
+	if( networkconnection == nullptr || networkconnection->IsServer() )
+	{
+		if( selection == Index && currentinfo->BlueA_Present )
+		{
+			btnimg = (BlueTeam ? GameResources::BlueButtonUp : GameResources::RedButtonUp);
+		} else if( currentinfo->BlueA_Present ) {
+			btnimg = (BlueTeam ? GameResources::BlueButtonUp : GameResources::RedButtonDown);
+		} else if( selection == Index ) {
+			btnimg = GameResources::WhiteButtonUp;
+		}
+	}
+	GameResources::DrawButton( btnimg, X, Y, 23, 3 );
 }
--- Game/gamelobbystage.h
+++ Game/gamelobbystage.h
@@ -3,13 +3,22 @@
 
 #include "includes.h"
 #include "resources.h"
+#include "gameinfo.h"
+#include "input.h"
 
 class GameLobbyStage : public Stage
 {
 
 	private:
 		TTFFont* optionfont;
+		TTFFont* helpfont;
 		Network* networkconnection;
+		GameInfo* currentinfo;
+		int selection;
+
+		void InputEvent(InputItems::ItemSet inputevent);
+		void NetworkEvent(Event* e);
+		void RenderPlayerSlot(bool BlueTeam, int Index, bool Present, std::string Name, bool Local, bool AI, int X, int Y);
 
   public:
 
